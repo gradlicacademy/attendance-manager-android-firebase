359e0a3a7de223bca0f88cc9e672f558250f41ed
Vikas Kumar <v.ray96610@gmail.com>
2020-08-09 12:31:13 +0530

    Revert "third commit with error to jump back"
    This reverts commit c144aeef5cff7a6ae7905a7c3f7db035ae932d51.

--- app/src/main/java/com/gradlic/advanceattendancemanager/MainActivity.kt
+++ app/src/main/java/com/gradlic/advanceattendancemanager/MainActivity.kt
@@ -10,8 +10,5 @@ class MainActivity : AppCompatActivity() {
 
         print("Test message for second git commit")
 
-
-        error in program to test revert with git this is a error line
-
     }
 }
